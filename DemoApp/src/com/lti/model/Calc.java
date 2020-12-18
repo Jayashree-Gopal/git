288b775dea749e270705340a0517657b0cf113fc
Jayashree Gopal <Jayashree Gopal@Jays-NoteBook>
2020-12-18 15:43:10 +0530

    Commit

--- DemoApp/src/com/lti/model/Calc.java
+++ DemoApp/src/com/lti/model/Calc.java
@@ -4,13 +4,4 @@ public class Calc {
 	public void add(int num1,int num2){
 		System.out.println("Sum="+num1+num2);
 	}
-	public void subs(int num1,int num2){
-		System.out.println("Difference="+(num1-num2));
-	}
-	public void print(String msg){
-		System.out.println(msg);
-	}
-	public void prod(int a,int b){
-		System.out.println("product="+a*b);
-	}
 }
